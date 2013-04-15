f514399b7601cae5bf649827c236b164e49bb880
ezralalonde <ezra@usefuliftrue.com>
2013-04-14 21:19:31 -0600

    go fmt'd code

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "yolo-octo-ironman/pat"
+	"fmt"
+	"yolo-octo-ironman/pat"
 )
 
 func main() {
-    var lines int
-    _, err := fmt.Scanf("%d", &lines)
-    if err != nil {
-        panic("could not read number of lines")
-    }
-    var kk, nn int
-    for ii := 0; ii < lines; ii++ {
-        _, err := fmt.Scanf("%d %d", &nn, &kk)
-        if err != nil {
-            panic("could not read n or k")
-        }
-        fmt.Printf("The bit patterns are\n")
-        bits := pat.GeneratePatterns(nn, kk)
-        for _, bit := range bits {
-            fmt.Println(bit)
-        }
-        fmt.Printf("\n")
-    }
+	var lines int
+	_, err := fmt.Scanf("%d", &lines)
+	if err != nil {
+		panic("could not read number of lines")
+	}
+	var kk, nn int
+	for ii := 0; ii < lines; ii++ {
+		_, err := fmt.Scanf("%d %d", &nn, &kk)
+		if err != nil {
+			panic("could not read n or k")
+		}
+		fmt.Printf("The bit patterns are\n")
+		bits := pat.GeneratePatterns(nn, kk)
+		for _, bit := range bits {
+			fmt.Println(bit)
+		}
+		fmt.Printf("\n")
+	}
 }
--- pat/pat.go
+++ pat/pat.go
@@ -1,28 +1,27 @@
 package pat
 
 import (
-    "strings"
+	"strings"
 )
 
 func GeneratePatterns(nn, kk int) []string {
-    aa := make([]string, nn)
-    var bb []string
-    Gen(nn, kk, aa, &bb)
-    return bb
+	aa := make([]string, nn)
+	var bb []string
+	Gen(nn, kk, aa, &bb)
+	return bb
 }
 
 func Gen(nn, kk int, aa []string, bb *[]string) {
-    if nn == 0 && kk == 0 {
-        *bb = append(*bb, strings.Join(aa, ""))
-    } else {
-        if kk > 0 {
-            aa[len(aa) - nn] = "1"
-            Gen(nn-1, kk - 1, aa, bb)
-        }
-        if nn > kk {
-            aa[len(aa) - nn] = "0"
-            Gen(nn-1, kk , aa, bb)
-        }
-    }
+	if nn == 0 && kk == 0 {
+		*bb = append(*bb, strings.Join(aa, ""))
+	} else {
+		if kk > 0 {
+			aa[len(aa)-nn] = "1"
+			Gen(nn-1, kk-1, aa, bb)
+		}
+		if nn > kk {
+			aa[len(aa)-nn] = "0"
+			Gen(nn-1, kk, aa, bb)
+		}
+	}
 }
-
